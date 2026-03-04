ddee0ea185cbcb8c039784d8c2791eef66e879f9
Fraser McCarter <114738535+spxllz@users.noreply.github.com>
2026-03-04 10:30:46 +0000

    Added chord creation

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from music_engine.engine import Controller
 #Global controller instance
 controller = Controller()
 
+from music_engine.core.chord import Chord
+
+c = Chord("C")
+print(c.get_chord_pitches()) #Expect something like [60, 64, 67]
+
+a = Chord("Amaj7")
+print(a.get_chord_pitches())
+
+
+
 #Track registration decorator
 def register_track(func):
     """Decorator to register a track setup function"""
@@ -19,47 +29,50 @@ def register_track(func):
 @register_track
 def track(piano):
     intro = piano.add_sequence("intro")
-    intro.add_note('C4', 0.5)
-    intro.add_note(64, 0.5, velocity=20)
-    intro.add_note(440.0, 1.0)
+    for pitch in a.get_chord_pitches():
+        intro.add_note(pitch, 0.5)
+
+    # intro.add_note('C4', 0.5)
+    # intro.add_note(64, 0.5, velocity=20)
+    # intro.add_note(440.0, 1.0)
     
-    verse = piano.add_sequence("verse")
-    verse.add_note('G3', 1)
-    verse.add_note('G3', 1)
-    verse.add_note('G3', 1)
+    # verse = piano.add_sequence("verse")
+    # verse.add_note('G3', 1)
+    # verse.add_note('G3', 1)
+    # verse.add_note('G3', 1)
 
 
-@register_track
-def track(guitar):
-    intro2 = guitar.add_sequence("intro2")
-    intro2.add_note('C4', 0.5)
-    intro2.add_note('E4', 0.5)
-    intro2.add_note('G4', 1.0)
+# @register_track
+# def track(guitar):
+#     intro2 = guitar.add_sequence("intro2")
+#     intro2.add_note('C4', 0.5)
+#     intro2.add_note('E4', 0.5)
+#     intro2.add_note('G4', 1.0)
 
-    verse2 = guitar.add_sequence("verse2")
-    verse2.generate_random_walk(start_pitch='C4', num_notes=16, max_step=2, duration=0.5, scale_type='major')
+#     verse2 = guitar.add_sequence("verse2")
+#     verse2.generate_random_walk(start_pitch='C4', num_notes=16, max_step=2, duration=0.5, scale_type='major')
 
-    verse2.wait(2)  #Add a rest before the next part
+#     verse2.wait(2)  #Add a rest before the next part
 
-    intro3 = guitar.add_sequence("intro3")
-    intro3.add_note('C4', 0.5)
-    intro3.add_note('Eb4', 0.5)
-    intro3.add_note('G4', 1.0)
+#     intro3 = guitar.add_sequence("intro3")
+#     intro3.add_note('C4', 0.5)
+#     intro3.add_note('Eb4', 0.5)
+#     intro3.add_note('G4', 1.0)
 
-    verse3 = guitar.add_sequence("verse3")
-    verse3.generate_random_walk(start_pitch='C4', num_notes=16, max_step=2, duration=0.5, scale_type='minor')
+#     verse3 = guitar.add_sequence("verse3")
+#     verse3.generate_random_walk(start_pitch='C4', num_notes=16, max_step=2, duration=0.5, scale_type='minor')
 
-#test for melodic rhythm generation
-@register_track
-def track(bass):
-    bass_seq = bass.add_sequence("bassline")
-    bass_seq.generate_melodic_rhythm(start_pitch=60, num_notes=16, allowed_durations=[0.25, 0.5, 1.0, 1.5, 2, 3], total_beats=8.0, scale_type='major', velocity=90)
+# #test for melodic rhythm generation
+# @register_track
+# def track(bass):
+#     bass_seq = bass.add_sequence("bassline")
+#     bass_seq.generate_melodic_rhythm(start_pitch=60, num_notes=16, allowed_durations=[0.25, 0.5, 1.0, 1.5, 2, 3], total_beats=8.0, scale_type='major', velocity=90)
 
-#test for just rhythm generation
-@register_track
-def track(drum):
-    drum_seq = drum.add_sequence("drum_pattern")
-    drum_seq.generate_rhythm_sequence(pitch=36, allowed_durations=[0.25, 0.5, 1.0, 1.5, 2, 3], total_beats=8.0, velocity=100)
+# #test for just rhythm generation
+# @register_track
+# def track(drum):
+#     drum_seq = drum.add_sequence("drum_pattern")
+#     drum_seq.generate_rhythm_sequence(pitch=36, allowed_durations=[0.25, 0.5, 1.0, 1.5, 2, 3], total_beats=8.0, velocity=100)
 
 
 def main():
@@ -82,9 +95,9 @@ def main():
     controller.show()
 
     #playback test
-    print("\n--- Playing bass track ---")
-    bass = tracks_by_name['bass']
-    bass.play(tempo=120)
+    print("\n--- Playing piano track ---")
+    piano = tracks_by_name['piano']
+    piano.play(tempo=120)
     #controller.export_midi("test_output.mid", tempo=120)
 
 
---
+++ music_engine/core/chord.py
@@ -0,0 +1,117 @@
+from music_engine.core.sequence import Note
+
+MAJOR_TRIAD_INTERVALS = [0, 4, 7]
+MINOR_TRIAD_INTERVALS = [0, 3, 7]
+DOMINANT_SEVENTH_INTERVALS = [0, 4, 7, 10]
+DIMINISHED_SEVENTH_INTERVALS = [0, 3, 6, 9]
+AUGMENTED_TRIAD_INTERVALS = [0, 4, 8]
+MAJOR_SEVENTH_INTERVALS = [0, 4, 7, 11]
+ADD_SIX_INTERVALS = [0, 4, 7, 9]
+ADD_NINE_INTERVALS = [0, 4, 7, 14]
+
+class Chord:
+    """
+    Represents a chord, which is a collection of notes played together.
+    """
+
+    def __init__(self, symbol, octave=4):
+        """
+        Symbol: A string representing the chord symbol.
+        
+        Valid chord symbols include:
+        - Major triads: "C", "Cmaj"
+        - Minor triads: "Cm", "Cmin"
+        - Dominant sevenths: "C7"
+        - Diminished sevenths: "Cdim7", "Cdim"
+        - Augmented triads: "Caug", "C+"
+        - Major sevenths: "Cmaj7", "CM7"
+        - Add six: "Cadd6"
+        - Add nine: "Cadd9", "Cadd2"
+        
+        Examples: "C", "Am", "G7", "Cmaj7", "Dm", "D7", "Edim7", "Caug"
+        
+        Octave: The octave in which to place the chord (default 4)
+        """
+
+        self.symbol = symbol
+        self.octave = octave
+
+        self.root_name, self.quality = self._parse_symbol(symbol)
+        self.root_midi = self._get_root_midi()
+
+    def _parse_symbol(self, symbol):
+        symbol = symbol.strip()
+
+        #Check for major seventh chords (maj7, M7)
+        if symbol.endswith('maj7') or symbol.endswith('M7'):
+            suffix = 'maj7' if symbol.endswith('maj7') else 'M7'
+            return symbol[:-len(suffix)], 'major_seventh'
+        
+        #Check for dominant seventh chords (7)
+        if symbol.endswith('7'):
+            return symbol[:-1], 'dominant_seventh'
+
+        #Check for diminished seventh chords (dim7, °7)
+        if symbol.endswith('dim7'):
+            return symbol[:-4], 'diminished_seventh'
+
+        #Check for diminished triads (dim, °)
+        if symbol.endswith('dim') or symbol.endswith('°'):
+            suffix = 'dim' if symbol.endswith('dim') else '°'
+            return symbol[:-len(suffix)], 'diminished_seventh'
+
+        #Check for augmented chords (aug, +)
+        if symbol.endswith('aug') or symbol.endswith('+'):
+            suffix = 'aug' if symbol.endswith('aug') else '+'
+            return symbol[:-len(suffix)], 'augmented'
+
+        #Check for add6 chords
+        if symbol.endswith('add6'):
+            return symbol[:-4], 'add_six'
+
+        #Check for add9/add2 chords
+        if symbol.endswith('add9') or symbol.endswith('add2'):
+            suffix = 'add9' if symbol.endswith('add9') else 'add2'
+            return symbol[:-len(suffix)], 'add_nine'
+
+        #Check for minor chords (m, min)
+        if symbol.endswith('m') and not symbol.endswith('maj'):
+            return symbol[:-1], 'minor'
+
+        if symbol.endswith('min'):
+            return symbol[:-3], 'minor'
+        
+        #Check for major chords (maj)
+        if symbol.endswith('maj'):
+            return symbol[:-3], 'major'
+        
+        #Default to major chord
+        return symbol, 'major'
+    
+    def _get_root_midi(self):
+        note_string = f"{self.root_name}{self.octave}"
+        note = Note(note_string, duration=1)  #Duration doesn't matter here
+        return note.midi_pitch
+    
+    def get_chord_pitches(self):
+        if self.quality == 'major':
+            intervals = MAJOR_TRIAD_INTERVALS
+        elif self.quality == 'minor':
+            intervals = MINOR_TRIAD_INTERVALS
+        elif self.quality == 'dominant_seventh':
+            intervals = DOMINANT_SEVENTH_INTERVALS
+        elif self.quality == 'diminished_seventh':
+            intervals = DIMINISHED_SEVENTH_INTERVALS
+        elif self.quality == 'augmented':
+            intervals = AUGMENTED_TRIAD_INTERVALS
+        elif self.quality == 'major_seventh':
+            intervals = MAJOR_SEVENTH_INTERVALS
+        elif self.quality == 'add_six':
+            intervals = ADD_SIX_INTERVALS
+        elif self.quality == 'add_nine':
+            intervals = ADD_NINE_INTERVALS
+        else:
+            raise ValueError(f"Unsupported chord quality: {self.quality}")
+
+        return [self.root_midi + interval for interval in intervals]
+        
